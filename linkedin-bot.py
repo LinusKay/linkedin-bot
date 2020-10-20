85775a861e291e290414f85fd53d6251bdaf42b0
Linus Kay <s3668218@student.rmit.edu.au>
2020-10-20 23:14:51 +1100

    cleaned up code. didn't leak token. job well done'

--- linkedin-bot.py
+++ linkedin-bot.py
@@ -44,61 +44,6 @@ async def share(ctx, link, *, arg = None):
 	else:
 		await ctx.send(user.mention + ' Please include profile name')
 
-
-linkedinchannels = [731321297497227327, 731829355675648050, 730655769363742750]
-@bot.command()
-async def broadcast(ctx, link, *, arg = None):
-	user = ctx.message.author
-	profile_link = link
-	profile_name = arg
-	guild = ctx.message.guild
-	if arg != None:
-		if profile_link.startswith('https://linkedin.com/in/') or profile_link.startswith('https://www.linkedin.com/in/'):
-			embed = discord.Embed(
-				title = 'Connect with ' + profile_name,
-				description = profile_name + " is looking for connections on LinkedIn! [View their profile.](" + profile_link + ")",
-				color = 0x32b3e2
-			)
-			embed.set_author(
-				name = "LinkedIn Profile",
-				url = "https://www.linkedin.com/in/linus-kay/",
-				icon_url = "https://techmarketingbuffalo.com/wp-content/uploads/2013/11/linkedin-logo-high-res-1254-1024x1024.jpg"
-			)
-			if guild.icon_url == None:
-				icon_url = 'https://libus.xyz/i/6a952652591d013cadd6b11f623c6339/upload.jpg'
-			else:
-				icon_url = guild.icon_url
-			embed.set_footer(text = 'Shared by ' + str(user) + ' in \'' + guild.name + '\'', icon_url = icon_url)
-			embed.set_thumbnail(url = user.avatar_url)
-			await ctx.send(embed = embed)
-
-			for channelid in linkedinchannels:
-				#ensure message isnt sent again in same channel
-				if channelid != ctx.channel.id:
-					embed = discord.Embed(
-						title = 'Connect with ' + profile_name,
-						description = profile_name + " is looking for connections on LinkedIn! [View their profile.](" + profile_link + ")",
-						color = 0x32b3e2
-					)
-					embed.set_author(
-						name = "LinkedIn Broadcast",
-						url = "https://www.linkedin.com/in/linus-kay/",
-						icon_url = "https://techmarketingbuffalo.com/wp-content/uploads/2013/11/linkedin-logo-high-res-1254-1024x1024.jpg"
-					)
-					if guild.icon_url == None:
-						icon_url = 'https://libus.xyz/i/6a952652591d013cadd6b11f623c6339/upload.jpg'
-					else:
-						icon_url = guild.icon_url
-					embed.set_footer(text = 'Shared by ' + str(user) + ' in \'' + guild.name + '\'', icon_url = icon_url)
-					embed.set_thumbnail(url = user.avatar_url)
-					channel = bot.get_channel(channelid)
-					await channel.send(embed = embed)
-			await ctx.message.delete()
-		else:
-			await ctx.send(user.mention + ' Profile url must start with https://linkedin.com/in/')
-	else:
-		await ctx.send(user.mention + ' Please include profile name')
-
 @bot.command()
 async def help(ctx):
 	embed = discord.Embed(
